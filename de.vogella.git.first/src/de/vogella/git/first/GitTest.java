60ddc10a7e7b19c2bf6a905b4c7f592f00f92da0
David Pridgen <dpridgen@manh.com>
2013-04-13 09:08:57 -0400

    test

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,7 +2,7 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is not coolaa");
+		System.out.println("Git is not coolaa test git hub");
 
 	}
 
